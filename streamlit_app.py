10d3b297c0ca6a4d74150edc648c4b0c4e4da847
Fuckhri911 <nooydking@gmail.com>
2025-05-21 14:14:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("INFORMATIKAvsINFANTRI")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("6.jpg", width=200)
